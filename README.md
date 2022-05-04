feb42e0be33c94003c40f7cdcb179ff4b1588dd0
aci686 <aaron.castro.sanchez@outlook.com>
2022-05-04 11:22:26 +0200

    Bashish TCP port scan

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 - lamp_install.sh       -> Install LAMP on a Debian system
 - notify_send_all.sh    -> Send global desktop notifications
 - ping.sh               -> Ping script with altered output
+- port_scan.sh          -> Check if a TCP port is open. Bashih /dev/tcp/...
 - progress_bar.sh       -> Command line progress bar
 - symbols.sh            -> Glyph scripts. Source this file
 - usage.sh              -> Usage information message. Source this file
---
+++ port_scan.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+#
+
+__author__="Aaron Castro"
+__author_email__="aaron.castro.sanchez@outlook.com"
+__copyright__="Aaron Castro"
+__license__="MIT"
+
+# Port scan
+# Uses echo to send empty strings to tcp ports
+# Checks whether port is open or it is not
+
+trap ctrl_c INT
+
+ctrl_c() {
+    echo "\n[!] Interrupted!\n"
+    tput cnorm
+    exit 1
+}
+
+usage() {
+    echo "Usage: $0 [-i <ip address>] [-p <tcp port number>]" 1>&2
+    exit 0
+}
+
+while getopts "i:p:" option; do
+    case $option in
+        p ) port="$OPTARG" ;;
+        i ) ip="$OPTARG" ;;
+        ? ) usage ;;
+    esac
+done
+
+if [ -z "$port" ] || [ -z "$ip" ]; then
+    usage
+fi
+
+tput civis
+timeout 1 bash -c "echo '' > /dev/tcp/$ip/$port" 2>/dev/null && echo "[*] Port $port - OPEN" &
+wait
+tput cnorm
