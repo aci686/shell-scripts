e8ab253b22221c014d7e919e0d9180885b4610b2
aci686 <aaron.castro.sanchez@outlook.com>
2022-05-04 10:42:05 +0200

    Real time CPU usage

---
+++ cpu_usage.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+#
+
+__author__="Aaron Castro"
+__author_email__="aaron.castro.sanchez@outlook.com"
+__copyright__="Aaron Castro"
+__license__="MIT"
+
+# Real time CPU usage
+
+while :; do
+  # Get the first line with aggregate of all CPUs
+  cpu_now=($(head -n1 /proc/stat))
+  # Get all columns but skip the first (which is the "cpu" string)
+  cpu_sum="${cpu_now[@]:1}"
+  # Replace the column seperator (space) with +
+  cpu_sum=$((${cpu_sum// /+}))
+  # Get the delta between two reads
+  cpu_delta=$((cpu_sum - cpu_last_sum))
+  # Get the idle time Delta
+  cpu_idle=$((cpu_now[4]- cpu_last[4]))
+  # Calc time spent working
+  cpu_used=$((cpu_delta - cpu_idle))
+  # Calc percentage
+  cpu_usage=$((100 * cpu_used / cpu_delta))
+  # Keep this as last for our next read
+  cpu_last=("${cpu_now[@]}")
+  cpu_last_sum=$cpu_sum
+  echo -ne "CPU usage at $cpu_usage%"\\r
+  # Wait a second before the next read
+  sleep 1
+done
