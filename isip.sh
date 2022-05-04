4f1a7b1ef16661232070a3a1b9f3d1e329b27409
aci686 <aaron.castro.sanchez@outlook.com>
2022-05-04 10:36:20 +0200

    Check if argument is an IP

---
+++ isip.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#
+
+__author__="Aaron Castro"
+__author_email__="aaron.castro.sanchez@outlook.com"
+__copyright__="Aaron Castro"
+__license__="MIT"
+
+# Symbol palette
+# Usage:
+# isip "213.200.123.34" -> Return 1 (True)
+# isip "311.12.23.34"   -> Return 0 (False)
+
+isip() {
+    if [[ $1 =~ ^([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$ ]]
+        then
+            return 1
+        else
+            return 0
+        fi
+}
