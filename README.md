c4a66b88a39fdaa04b180c2618bd8ed21dd3821e
aci686 <aaron.castro.sanchez@outlook.com>
2022-05-04 10:04:19 +0200

    Progress bar

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## shell-scripts
 
 - colors.sh -> Colorize scripts. Source this file.
+- progress_bar -> Command line progress bar.
---
+++ progress_bar.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#
+
+__author__="Aaron Castro"
+__author_email__="aaron.castro.sanchez@outlook.com"
+__copyright__="Aaron Castro"
+__license__="MIT"
+
+# Progress bar
+
+for ((k = 0; k <= 10 ; k++)); do
+        echo -n "["
+        for ((i = 0; i <= k; i++)); do
+            echo -n "###"
+        done
+        for ((j = i; j <= 10; j++)); do
+            echo -n "   "
+        done
+        v=$((k * 10))
+        echo -n "] "
+        echo -n "$v %" $'\r'
+        sleep 0.70
+    done
+echo
