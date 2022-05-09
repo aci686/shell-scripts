cfa445e8d746bdb8c2df474b5335da23f30537b9
Aaron <56502347+aci686@users.noreply.github.com>
2022-05-09 12:15:28 +0200

    Added random animations

--- spinner.sh
+++ spinner.sh
@@ -10,15 +10,42 @@ __license__="MIT"
 # Spinner progress indicator
 # Usage:
 # Add the following at the beginning of your slow function:
+#   tput civis
 #   spinner &
 #   spin_pid=$!
 #   trap "kill -9 $spin_pid 2>/dev/null" $(seq 0 15)
 # And add the following a the end of the slow function:
 #   kill -9 $spin_pid 2>/dev/null
+#   tput cnorm
 
 spinner() {
     local i sp n
-    sp='/-\|'
+    case $((RANDOM % 12 )) in
+    0)
+        sp='-\|/' ;;
+    1)
+        sp='⠁⠂⠄⡀⢀⠠⠐⠈' ;;
+    2)
+        sp='▉▊▋▌▍▎▏▎▍▌▋▊▉' ;;
+    3)
+        sp='▁▂▃▄▅▆▇█▇▆▅▄▃▂▁' ;;
+    4)
+        sp='←↖↑↗→↘↓↙' ;;
+    5)
+        sp='┤┘┴└├┌┬┐' ;;
+    6)
+        sp='▖▘▝▗' ;;
+    7)
+        sp='⣾⣽⣻⢿⡿⣟⣯⣷' ;;
+    8)
+        sp='◴◷◶◵' ;;
+    9)
+        sp='◰◳◲◱' ;;
+    10)
+        sp='◐◓◑◒' ;;
+    11)
+        sp='◢◣◤◥' ;;
+esac
     n=${#sp}
     while sleep 0.333
     do
