0b46e0d8e8a17c1afda05e0be3a922edb8933488
aci686 <aaron.castro.sanchez@outlook.com>
2022-06-09 09:51:22 +0200

    Change backup script to rsync

--- back_me_up.sh
+++ back_me_up.sh
@@ -8,40 +8,27 @@ __author_nick__="i686"
 __copyright__="Aaron Castro"
 __license__="MIT"
 
-# Backs up $HOME to a specified destination CIFS drive
+# Backs up $HOME to a specified rsync destination
 
 usage() {
-    echo '[i] Usage: $0' 1>&2
+    echo "[i] Usage: $0 <username> <destination>" 1>&2
     exit 0
 }
 
 ctrl_c() {
-    echo -e '\n[!] Aborting...'
+    echo -e "\n[!] Aborting..."
     tput cnorm
     exit 1
 }
 
 trap ctrl_c INT
 
-DESTINATION='insert here your destination name' # will mount //$DESTINATION/$(whoami)/
-
-echo '[i] Checking destination mountpoint...'
-mount | grep $DESTINATION &> /dev/null
-if [ $? == 0 ]; then
-    echo '[!] Already mounted...'
-    t_mtp=$(mount | grep $DESTINATION | awk '{print $3}')
-    echo '[i] Reusing '$t_mtp'...'
-else
-    echo '[i] Creating temp mountpoint...'
-    t_mtp=$(mktemp -d)
-    echo '[i] '$t_mtp' created...'
-    echo '[i] Mounting remote filesystem...'
-    sudo mount -t cifs //$DESTINATION/$(whoami)/ $t_mtp -o username=$(whoami),uid=$(id -u),gid=$(id -g)
+if [ $# -ne 2 ]; then
+    usage
 fi
-echo '[i] Starting sync...'
-rsync -aXv --omit-dir-times --backup-dir=versions --suffix="."$(date +"%d-%m-%Y") --ignore-errors --info=ALL --log-file=backup.$(date +"%d-%m-%Y").log --progress $HOME/ --exclude={".cache"} $t_mtp
-echo '[OK] Sync complete!'
-echo '[i] Unmounting...'
-sudo umount $t_mtp
+
+echo "[i] Starting sync..."
+rsync -aXv --omit-dir-times --backup-dir=versions --suffix="."$(date +"%d-%m-%Y") --ignore-errors --no-perms --info=ALL --log-file=backup.$(date +"%d-%m-%Y").log --progress $HOME/ --exclude={"$HOME/.cache"} $1@$2
+echo "[OK] Sync complete!"
 
 tput cnorm
